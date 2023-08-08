77decffc5ce5b2f1ffd5620ba21a7cc58e8f5f4b
Brian Kroth <bpkroth@microsoft.com>
2023-08-08 01:59:42 +0000

    generalize the toy examples test case

--- mlos_bench/mlos_bench/tests/optimizers/toy_optimization_loop_test.py
+++ mlos_bench/mlos_bench/tests/optimizers/toy_optimization_loop_test.py
@@ -6,15 +6,20 @@
 Toy optimization loop to test the optimizers on mock benchmark environment.
 """
 
-from typing import Tuple
+from typing import List, Tuple
 
 import pytest
+from pytest import FixtureRequest
+from pytest_lazyfixture import lazy_fixture, LazyFixture
+
+from mlos_core.tests import get_all_concrete_subclasses
+from mlos_core.optimizers import OptimizerType
 
 from mlos_bench.environments.base_environment import Environment
 from mlos_bench.environments.mock_env import MockEnv
 from mlos_bench.tunables.tunable_groups import TunableGroups
 from mlos_bench.optimizers.base_optimizer import Optimizer
-from mlos_bench.optimizers.mock_optimizer import MockOptimizer
+from mlos_bench.optimizers.one_shot_optimizer import OneShotOptimizer
 from mlos_bench.optimizers.mlos_core_optimizer import MlosCoreOptimizer
 
 
@@ -44,61 +49,67 @@ def _optimize(env: Environment, opt: Optimizer) -> Tuple[float, TunableGroups]:
     return (best_score, best_tunables)
 
 
-def test_mock_optimization_loop(mock_env_no_noise: MockEnv,
-                                mock_opt: MockOptimizer) -> None:
-    """
-    Toy optimization loop with mock environment and optimizer.
-    """
-    (score, tunables) = _optimize(mock_env_no_noise, mock_opt)
-    assert score == pytest.approx(64.9, 0.01)
-    assert tunables.get_param_values() == {
+# (optimizer, expected_score, expected_tunable_values)
+OPTIMIZER_TEST_CASES: List[Tuple[LazyFixture, float, dict]] = [
+    (lazy_fixture("mock_opt"), 64.9, {
         "vmSize": "Standard_B2ms",
         "idle": "halt",
         "kernel_sched_migration_cost_ns": 117025,
         "kernel_sched_latency_ns": 149827706,
-    }
-
-
-def test_mock_optimization_loop_no_defaults(mock_env_no_noise: MockEnv,
-                                            mock_opt_no_defaults: MockOptimizer) -> None:
-    """
-    Toy optimization loop with mock environment and optimizer.
-    """
-    (score, tunables) = _optimize(mock_env_no_noise, mock_opt_no_defaults)
-    assert score == pytest.approx(60.97, 0.01)
-    assert tunables.get_param_values() == {
+    }),
+    (lazy_fixture("mock_opt_no_defaults"), 60.97, {
         "vmSize": "Standard_B2s",
         "idle": "halt",
         "kernel_sched_migration_cost_ns": 49122,
         "kernel_sched_latency_ns": 234760738,
-    }
-
-
-def test_flaml_optimization_loop(mock_env_no_noise: MockEnv,
-                                 flaml_opt: MlosCoreOptimizer) -> None:
-    """
-    Toy optimization loop with mock environment and FLAML optimizer.
-    """
-    (score, tunables) = _optimize(mock_env_no_noise, flaml_opt)
-    assert score == pytest.approx(75.0, 0.01)
-    assert tunables.get_param_values() == {
+    }),
+    (lazy_fixture("flaml_opt"), 75.0, {
         "vmSize": "Standard_B4ms",
         "idle": "halt",
         "kernel_sched_migration_cost_ns": -1,
         "kernel_sched_latency_ns": 2000000,
-    }
-
-
-def test_smac_optimization_loop(mock_env_no_noise: MockEnv,
-                                smac_opt: MlosCoreOptimizer) -> None:
-    """
-    Toy optimization loop with mock environment and SMAC optimizer.
-    """
-    (score, tunables) = _optimize(mock_env_no_noise, smac_opt)
-    assert score == pytest.approx(67.6, 0.01)
-    assert tunables.get_param_values() == {
+    }),
+    (lazy_fixture("smac_opt"), 67.6, {
         "vmSize": "Standard_B2ms",
         "idle": "mwait",
         "kernel_sched_migration_cost_ns": 37322,
         "kernel_sched_latency_ns": 40128951,
-    }
+    }),
+]
+
+
+@pytest.mark.parametrize("opt, expected_score, expected_tunable_values", OPTIMIZER_TEST_CASES)
+def test_optimization_loop(mock_env_no_noise: MockEnv,
+                           opt: Optimizer,
+                           expected_score: float,
+                           expected_tunable_values: dict) -> None:
+    """
+    Toy optimization loop with mock environment and optimizer.
+    """
+    (score, tunables) = _optimize(mock_env_no_noise, opt)
+    assert score == pytest.approx(expected_score, 0.01)
+    assert tunables.get_param_values() == expected_tunable_values
+
+
+def test_optimizer_coverage(request: FixtureRequest) -> None:
+    """
+    Make sure that all optimizers are tested.
+    """
+    mlos_core_optimizer_types: List[OptimizerType] = [
+        opt_type for opt_type in OptimizerType
+        if opt_type not in [OptimizerType.RANDOM]     # Ignore RANDOM
+    ]
+    mlos_bench_optimizer_types = [
+        opt_cls for opt_cls in get_all_concrete_subclasses(Optimizer)
+        if opt_cls not in [OneShotOptimizer]    # Ignore OneShotOptimizer
+    ]
+    for (lazy_opt, _, _) in OPTIMIZER_TEST_CASES:
+        opt = request.getfixturevalue(lazy_opt.name)
+        assert isinstance(opt, Optimizer)
+        if opt.__class__ in mlos_bench_optimizer_types:
+            mlos_bench_optimizer_types.remove(opt.__class__)
+        if isinstance(opt, MlosCoreOptimizer):
+            if opt.optimizer_type in mlos_core_optimizer_types:
+                mlos_core_optimizer_types.remove(opt.optimizer_type)
+    assert len(mlos_core_optimizer_types) == 0, f"Missing tests for {mlos_core_optimizer_types}"
+    assert len(mlos_bench_optimizer_types) == 0, f"Missing tests for {mlos_bench_optimizer_types}"
