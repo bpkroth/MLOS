b5b6c037ac4a8d45cba93ee27f392a8c42aa411c
Brian Kroth <bpkroth@microsoft.com>
2024-06-07 22:27:36 +0000

    wip: more sanity checks on wheels

---
+++ scripts/sanity-check-whl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+#set -x
+
+WHL="$1"
+
+whl_metadata=$(unzip -p "$WHL" "*/METADATA")
+
+# Get all non-full extras reported by wheel.
+provides_extras=$(echo "$whl_metadata" | grep ^Provides-Extra: | grep -v ': full' | awk '{ print $2 }' | sort)
+# Make sure they're all listsed in the "full" extra.
+full_requires=$(echo "$whl_metadata" | grep ^Requires-Dist: | grep "extra == 'full'" | sed -r -e "s/^Requires-Dist: mlos[a-z_-]+\[([a-zA-Z0-9,-]+)\] ; extra == 'full'$/\1/" | sed -e 's/,/\n/g' | sort)
+
+if [ "$provides_extras" != "$full_requires" ]; then
+	echo -e "ERROR: Mismatched full target requires vs. provides_extras for $WHL.\n" >&2
+	echo -e "Provides Extras:\n\n$provides_extras" >&2
+	echo -e "Full Requires:\n\n$full_requires" >&2
+fi
+
+# OK
+exit 0
