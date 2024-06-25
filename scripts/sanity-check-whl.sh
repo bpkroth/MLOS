4d7b5558ffdff0e711636b99d9f1ff5a3bccecbf
Brian Kroth <bpkroth@microsoft.com>
2024-06-25 21:43:05 +0000

    die after errors are found

--- scripts/sanity-check-whl.sh
+++ scripts/sanity-check-whl.sh
@@ -14,9 +14,10 @@ provides_extras=$(echo "$whl_metadata" | grep ^Provides-Extra: | grep -v ': full
 full_requires=$(echo "$whl_metadata" | grep ^Requires-Dist: | grep "extra == 'full'" | sed -r -e "s/^Requires-Dist: mlos[a-z_-]+\[([a-zA-Z0-9,-]+)\] ; extra == 'full'$/\1/" | sed -e 's/,/\n/g' | sort)
 
 if [ "$provides_extras" != "$full_requires" ]; then
-	echo -e "ERROR: Mismatched full target requires vs. provides_extras for $WHL.\n" >&2
-	echo -e "Provides Extras:\n\n$provides_extras" >&2
-	echo -e "Full Requires:\n\n$full_requires" >&2
+    echo -e "ERROR: Mismatched full target requires vs. provides_extras for $WHL.\n" >&2
+    echo -e "Provides Extras:\n\n$provides_extras\n" >&2
+    echo -e "Full Requires:\n\n$full_requires\n" >&2
+    exit 1
 fi
 
 # OK
